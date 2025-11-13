d94db781eb94a36cb6cb02ab6b1c96e316358244
Dhruv Kandula <dhruv.kandula@gmail.com>
2025-11-13 15:57:45 -0800

    Implement HGET and HSET

--- redis/handler.go
+++ redis/handler.go
@@ -17,6 +17,13 @@ type Database struct {
 
 var db = Database{entries: map[string]string{}}
 
+type HashDatabase struct {
+	entries map[string]map[string]string
+
+	mutex sync.RWMutex
+}
+
+var hashDB = HashDatabase{entries: map[string]map[string]string{},}
 
 
 var Handlers = map[string]func([]resp.Value) (resp.Value, error) {
@@ -24,6 +31,8 @@ var Handlers = map[string]func([]resp.Value) (resp.Value, error) {
 	"PING": handlePing,
 	"SET": handleSet,
 	"GET": handleGet,
+	"HSET": handleHSet,
+	"HGET": handleHGet,
 }
 
 func handleCommand(args []resp.Value) (resp.Value, error) {
@@ -73,6 +82,51 @@ func handleGet(args []resp.Value) (resp.Value, error) {
 		return resp.Value{Typ:resp.TypeNull}, nil
 	}
 
+
+	return resp.Value{Typ:resp.TypeBulk, Bulk: val}, nil
+}
+
+func handleHSet(args []resp.Value) (resp.Value, error) {
+	if len(args) != 3 {
+		return resp.Value{}, fmt.Errorf("Invalid Number of arguments")
+	}
+	if args[0].Typ != resp.TypeBulk || args[1].Typ != resp.TypeBulk {
+		return resp.Value{}, fmt.Errorf("Invalid argument types. Must be Bulk Strings")
+	}
+
+	key := args[0].Bulk
+	field := args[1].Bulk
+	val := args[2].Bulk
+
+	hashDB.mutex.Lock()
+	_, exists := hashDB.entries[key]
+	if !exists {
+		hashDB.entries[key] = map[string]string{}
+	}
+	hashDB.entries[key][field] = val
+	hashDB.mutex.Unlock()
+
+	return resp.Value{Typ:resp.TypeString, Str: "OK"}, nil
+}
+
+func handleHGet(args []resp.Value) (resp.Value, error) {
+	if len(args) != 2{
+		return resp.Value{}, fmt.Errorf("Invalid Number of arguments")
+	}
+	if args[0].Typ != resp.TypeBulk {
+		return resp.Value{}, fmt.Errorf("Invalid argument types. Must be Bulk Strings")
+	}
+
+	key := args[0].Bulk
+	field := args[1].Bulk
+
+	hashDB.mutex.RLock()
+	val, exists := hashDB.entries[key][field]
+	hashDB.mutex.RUnlock()
+	
+	if !exists {
+		return resp.Value{Typ:resp.TypeNull}, nil
+	}
 
 	return resp.Value{Typ:resp.TypeBulk, Bulk: val}, nil
 }
