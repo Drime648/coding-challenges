965f888cef3a1c5d23282816e82aa16e8227079d
Dhruv Kandula <dhruv.kandula@gmail.com>
2025-11-24 16:53:09 -0800

    add initial encoding logic

--- huffman-encoder/internal/huffman/encoder.go
+++ huffman-encoder/internal/huffman/encoder.go
@@ -1,18 +1,54 @@
 package huffman
 
-func buildHeap(freq map[byte]int) *NodeHeap {
-	heap := &NodeHeap{}
-	for k, v := range freq {
-		n := Node{count: v, data: k}
-		heap.Push(n)
+import "io"
+
+func buildPrefixTree(root *Node) map[byte]string {
+	prefixTree := map[byte]string{}
+	dfs(root, prefixTree, "")
+	return prefixTree
+}
+
+func dfs(node *Node, prefixTree map[byte]string, currBitString string) {
+	if node.left == nil && node.right == nil {
+		prefixTree[node.data] = currBitString
 	}
 
-	return heap
+	if node.right != nil {
+		dfs(node.right, prefixTree, currBitString+"1")
+	}
+
+	if node.left != nil {
+		dfs(node.left, prefixTree, currBitString+"0")
+	}
 }
 
-// build a heap first??
-func buildEncoderTree(freq map[byte]int) Node {
-	nodeHeap := buildHeap(freq)
+func encodeData(r io.Reader, w io.Writer) error {
+	frequencyTable, err := CountFrequency(r)
+	if err != nil {
+		return err
+	}
+	root := buildEncoderTree(frequencyTable)
+	prefixTree := buildPrefixTree(&root)
+	data := make([]byte, 4096)
+	for {
+		n, err := r.Read(data)
+		if err != nil {
+			if err == io.EOF {
+				break
+			} else {
+				return err
+			}
+		}
+		for i := range n {
+			b := data[i]
+			bitString := prefixTree[b]
 
-	return Node{}
+		}
+	}
+
+	return nil
 }
+
+
+
+func bitStringTobit(:)
--- huffman-encoder/internal/huffman/tree.go
+++ huffman-encoder/internal/huffman/tree.go
@@ -1,8 +1,38 @@
 package huffman
 
+import (
+	"container/heap"
+)
+
 type Node struct {
 	count int
 	data  byte
 	left  *Node
 	right *Node
+}
+
+func buildHeap(freq map[byte]int) *NodeHeap {
+	h := &NodeHeap{}
+	heap.Init(h)
+	for k, v := range freq {
+		n := Node{count: v, data: k}
+		heap.Push(h, n)
+	}
+
+	return h
+}
+
+func buildEncoderTree(freq map[byte]int) Node {
+	nodeHeap := buildHeap(freq)
+
+	for nodeHeap.Len() > 1 {
+		n1 := heap.Pop(nodeHeap).(Node)
+		n2 := heap.Pop(nodeHeap).(Node)
+
+		sum := n1.count + n2.count
+		newNode := Node{count: sum, left: &n1, right: &n2}
+		heap.Push(nodeHeap, newNode)
+	}
+
+	return heap.Pop(nodeHeap).(Node)
 }
