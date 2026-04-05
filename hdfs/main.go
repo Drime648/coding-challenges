ce494fb895b0fd6d93803c76ca7b8276495da2d9
Dhruv Kandula <dhruv.kandula@gmail.com>
2026-04-05 10:19:27 -0700

    Implement basic file server loop

--- hdfs/main.go
+++ hdfs/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"log"
+	"time"
 
 	"github.com/Drime648/coding-challenges/hdfs/p2p"
 )
@@ -13,24 +13,32 @@ func onPeer(p2p.Peer) error {
 }
 
 func main() {
-	tcpOpts := p2p.TCPTransportOpts{
+
+	tcpTransportOpts := p2p.TCPTransportOpts{
 		ListenAddr:    ":3000",
 		HandshakeFunc: p2p.NopHandshakeFunc,
 		Decoder:       p2p.DefaultDecoder{},
-		OnPeer:        onPeer,
+		// OnPeer: onPeer,
 	}
-	tr := p2p.NewTCPTransport(tcpOpts)
+	tcpTransport := p2p.NewTCPTransport(tcpTransportOpts)
+
+	fileServerOpts := FileServerOpts{
+		StorageRoot:       "3000_files",
+		PathTransformFunc: CASPathTransformFun,
+		Transport:         tcpTransport,
+	}
+	s := NewFileServer(fileServerOpts)
 
 	go func() {
-		for {
-			msg := <-tr.Consume()
-			fmt.Printf("%+v\n", msg)
-		}
+		time.Sleep(3 * time.Second)
+		s.Stop()
 	}()
 
-	if err := tr.ListenAndAccept(); err != nil {
-		log.Fatal(err)
+	if err := s.Start(); err != nil {
+		panic(err)
 	}
 
-	select {}
+
+
+
 }
--- hdfs/p2p/tcp_transport.go
+++ hdfs/p2p/tcp_transport.go
@@ -2,6 +2,7 @@ package p2p
 
 import (
 	"fmt"
+	"log"
 	"net"
 )
 
@@ -58,6 +59,7 @@ func (t *TCPTransport) ListenAndAccept() error {
 	}
 
 	go t.startAcceptLoop()
+	log.Printf("TCP Transport listening on port: %s\n", t.ListenAddr)
 	return nil
 }
 
