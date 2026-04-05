a9e97dff6a6291773cb309400699a4ce85793b50
Dhruv Kandula <dhruv.kandula@gmail.com>
2026-04-04 17:57:50 -0700

    Implemented CAS Path transform function

--- hdfs/store_test.go
+++ hdfs/store_test.go
@@ -3,14 +3,18 @@ package main
 import (
 	"bytes"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
-// func TestPathTransformFunc(t *testing.T) {
-
-// }
+func TestPathTransformFunc(t *testing.T) {
+	key := "bestpic"
+	pathname := CASPathTransformFun(key)
+	assert.Equal(t, "6f90c/0cbff/d1b2a/a1e69/c839a/5b960/6ff14/5c565", pathname)
+}
 
 func TestStore(t *testing.T) {
-	opts := StoreOpts {
+	opts := StoreOpts{
 		PathTransformFunc: DefaultPathTransformFunc,
 	}
 	s := NewStore(opts)
