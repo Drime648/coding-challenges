4fc0631e18a603d43b637659012916fb3f201fb7
Dhruv Kandula <dhruv.kandula@gmail.com>
2026-04-04 17:59:16 -0700

    refactor tests to use assert

--- hdfs/store_test.go
+++ hdfs/store_test.go
@@ -10,17 +10,16 @@ import (
 func TestPathTransformFunc(t *testing.T) {
 	key := "bestpic"
 	pathname := CASPathTransformFun(key)
-	assert.Equal(t, "6f90c/0cbff/d1b2a/a1e69/c839a/5b960/6ff14/5c565", pathname)
+	expectedPathName := "6f90c/0cbff/d1b2a/a1e69/c839a/5b960/6ff14/5c565"
+	assert.Equal(t, expectedPathName, pathname)
 }
 
 func TestStore(t *testing.T) {
 	opts := StoreOpts{
-		PathTransformFunc: DefaultPathTransformFunc,
+		PathTransformFunc: CASPathTransformFun,
 	}
 	s := NewStore(opts)
 
 	data := bytes.NewReader([]byte("some data"))
-	if err := s.writeStream("test_file", data); err != nil {
-		t.Error(err)
-	}
+	assert.Nil(t, s.writeStream("test_file", data))
 }
