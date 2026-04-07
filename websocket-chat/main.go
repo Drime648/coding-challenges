fc31d722989741f0f2d2d184c93a49895d32fb0d
Dhruv Kandula <dhruv.kandula@gmail.com>
2026-04-06 22:13:00 -0700

    Modified server to use channels instead of locks to handle race conditions

--- websocket-chat/main.go
+++ websocket-chat/main.go
@@ -14,38 +14,41 @@ var (
 	WSPort = ":3223"
 )
 
-
 type Client struct {
 	ID string
 
-	mu *sync.RWMutex
+	mu   *sync.RWMutex
 	conn *websocket.Conn
 }
 
 func NewClient(conn *websocket.Conn) *Client {
 	ID := rand.Text()[:9]
 	return &Client{
-		ID: ID,
-		mu: new(sync.RWMutex),
+		ID:   ID,
+		mu:   new(sync.RWMutex),
 		conn: conn,
 	}
 }
 
 type Server struct {
-	mu      *sync.RWMutex
-	clients []*Client
+	mu            *sync.RWMutex
+	clients       map[string]*Client
+	joinServerCH  chan *Client
+	leaveServerCH chan *Client
 }
 
 func NewServer() *Server {
 	return &Server{
-		mu: new(sync.RWMutex),
-		clients: []*Client{},
+		mu:      new(sync.RWMutex),
+		clients: map[string]*Client{},
+		joinServerCH: make(chan *Client, 64),
+		leaveServerCH: make(chan *Client, 64),
 	}
 }
 
 func (s *Server) HandleWS(w http.ResponseWriter, r *http.Request) {
-	upgrader := websocket.Upgrader {
-		ReadBufferSize: 512,
+	upgrader := websocket.Upgrader{
+		ReadBufferSize:  512,
 		WriteBufferSize: 512,
 		CheckOrigin: func(r *http.Request) bool {
 			return true
@@ -59,14 +62,35 @@ func (s *Server) HandleWS(w http.ResponseWriter, r *http.Request) {
 	}
 
 	client := NewClient(conn)
+	s.joinServerCH <- client
+}
 
-	s.mu.Lock()
-	s.clients = append(s.clients, client)
-	s.mu.Unlock()
+func (s *Server) AcceptLoop() {
+	for {
+		select {
+		case c := <- s.joinServerCH:
+			//handle join logic. When a client joins server, this
+			//channel is filled.
+			s.joinServer(c)
+		case c := <- s.leaveServerCH:
+			s.leaveServer(c)
+		}
+	}
+}
+
+func(s *Server) joinServer(c *Client){
+	s.clients[c.ID] = c
+	fmt.Printf("client joined server, cID = %s\n", c.ID)
+}
+
+func (s *Server) leaveServer(c *Client) {
+	delete(s.clients, c.ID)
+	fmt.Printf("client left server, cID = %s\n", c.ID)
 }
 
 func CreateWSServer() {
 	s := NewServer()
+	go s.AcceptLoop()
 	http.HandleFunc("/", s.HandleWS)
 
 	fmt.Printf("Starting server on port: %s\n", WSPort)
