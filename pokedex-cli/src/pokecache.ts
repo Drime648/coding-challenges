6972e40d515e9ed94c029fe5a5bd6c184a3ee2bc
Dhruv Kandula <dhruv.kandula@gmail.com>
2026-05-07 22:42:27 -0700

    implement basic get and set for generic cache

---
+++ pokedex-cli/src/pokecache.ts
@@ -0,0 +1,24 @@
+
+
+type CacheEntry<T> = {
+  createdAt: number;
+  val: T;
+}
+
+export class Cache {
+  #cache = new Map<string, CacheEntry<any>>();
+
+  add<T>(key: string, val: T) {
+    const entry: CacheEntry<T> = {
+      createdAt: Date.now(),
+      val: val,
+    };
+    this.#cache.set(key, entry);
+  }
+
+  get<T>(key: string): T | undefined {
+    const entry = this.#cache.get(key);
+    return entry?.val
+  }
+}
+
